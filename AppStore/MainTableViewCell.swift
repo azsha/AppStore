9078c2846006a820d11003096241951a3b6bbc3b
azsha <azsha@naver.com>
2018-11-02 23:44:14 +0900

    커밋 테스트

--- AppStore/MainTableViewCell.swift
+++ AppStore/MainTableViewCell.swift
@@ -33,7 +33,5 @@ class MainTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 }
